3ceb94557877fa6305b52193e54c0934c7bb0279
Christopher Cabrera <ccabrera622@gmail.com>
2020-09-14 11:30:15 -0400

    added week 5

---
+++ Week5/Day1/Week5Day1HW/Week5Day1HW/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5Day1HW
+{
+    class Person
+    {
+        protected int age;
+        public void Greet()
+        {
+            Console.WriteLine("Hello");
+        }
+        public void SetAge(int n)
+        {
+            age = n;
+        }
+    }
+
+    class Student : Person
+    {
+        public void GoToClass()
+        {
+            Console.WriteLine("I'm going to class.");
+        }
+        public void ShowAge()
+        {
+            Console.WriteLine("My age is: " + this.age + " years old");
+        }
+    }
+
+    class Teacher : Person
+    {
+        private string subject;
+        public void Explain()
+        {
+            Console.WriteLine("Explanation begins");
+        }
+    }
+
+    class StudentAndTeacherTest
+    {
+        static void Main(string[] args)
+        {
+            Person per = new Person();
+            per.Greet();
+            Console.WriteLine();
+
+            Student std = new Student();
+            std.SetAge(21);
+            std.Greet();
+            std.ShowAge();
+            Console.WriteLine();
+
+            Teacher tch = new Teacher();
+            tch.SetAge(30);
+            tch.Greet();
+            tch.Explain();
+            Console.WriteLine();
+        }
+    }
+    //class Program
+    //{
+        
+    //}
+}
