2a3ac86109efd0773f4242e328b51fb2f46723ca
Christopher Cabrera <ccabrera622@gmail.com>
2020-09-28 12:01:16 -0400

    Updated week 6 and 7

---
+++ Week7/Day1/OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    }
+
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        //new property restaurants
+        readonly List<Restaurant> restaurants;
+
+        //constructor
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Scott's Pizza", Location="Maryland", Cuisine=CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Cinnamon Club", Location="London", Cuisine=CuisineType.Italian},
+                new Restaurant { Id = 3, Name = "La Costa", Location = "California", Cuisine=CuisineType.Mexican}
+            };
+        }
+
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                       //add where operator will return all if null or just by name
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
