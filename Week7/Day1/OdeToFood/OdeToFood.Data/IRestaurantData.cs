73c9c9e13aaf7b4a0d58f9f51b55074c8af83ff5
Christopher Cabrera <ccabrera622@gmail.com>
2020-09-29 10:55:34 -0400

    updated ode to food

--- Week7/Day1/OdeToFood/OdeToFood.Data/IRestaurantData.cs
+++ Week7/Day1/OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -11,15 +11,20 @@ namespace OdeToFood.Data
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+
+        //New Methods added to interface
+        Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
+        int Commit();
+
     }
 
 
     public class InMemoryRestaurantData : IRestaurantData
     {
-        //new property restaurants
         readonly List<Restaurant> restaurants;
 
-        //constructor
         public InMemoryRestaurantData()
         {
             restaurants = new List<Restaurant>()
@@ -30,14 +35,44 @@ namespace OdeToFood.Data
             };
         }
 
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
 
         public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
         {
             return from r in restaurants
-                       //add where operator will return all if null or just by name
                    where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
                    orderby r.Name
                    select r;
+        }
+
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if (restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+
+
+        public int Commit()
+        {
+            return 0;
+        }
+
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            restaurants.Add(newRestaurant);
+            return newRestaurant;
         }
     }
 }
---
+++ Week7/Day1/OdeToFood/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Data;
+using OdeToFood.Core;
+
+namespace OdeToFood.Pages.Restaurants
+{
+
+
+
+    public class DetailModel : PageModel
+    {
+        public Restaurant Restaurant { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        private readonly IRestaurantData restaurantData;
+        public DetailModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        //adjust OnGet() here
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
